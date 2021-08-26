f919d29f0004fd55fb4d4aa8f932d8600496ee1b
박지원 <mz_jwp@woowafriends.com>
2021-08-26 14:46:34 +0900

    [프로그래머스] 구명보트 완료

--- practice/src/programmers/level2/Lifeboat.java
+++ practice/src/programmers/level2/Lifeboat.java
@@ -1,7 +1,21 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
 public class Lifeboat {
     public static void main(String[] args) {
-        System.out.println(11);
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+        int result = 0;
+        int minIndex = 0;
+        Arrays.sort(people);
+        for (int i = people.length-1; minIndex < i; i--) {
+            int sum = people[minIndex] + people[i];
+            if (sum <= limit) {
+                minIndex++;
+            }
+            result++;
+        }
+        System.out.println(result);
     }
 }
